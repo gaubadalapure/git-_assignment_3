1b3b919c858a18fff3030f6666fd739470f532de
gaubadalapure <gaubadalapure@gmail.com>
2022-09-27 10:03:33 +0530

    prime problem

---
+++ prime.js
@@ -0,0 +1,15 @@
+function prime(num){
+  let factors=0;
+  for(let i=0;i<=num;i++){
+    if(num%i==0){
+      factors++;
+    }
+  }
+  if(factors==2){
+    console.log("Yes");
+  }
+  else{
+    console.log("not a prime");
+  }
+}
+prime(13);
